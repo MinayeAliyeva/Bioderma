4d794fce19904ece654d3098099ff90faf5c9a08
MinayeAliyeva <7s346pu@code.edu.az>
2023-11-11 23:11:35 +0400

    added hero section

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log("jjj");
+$('#hero_carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    items: 1,
+    mouseDrag:false,
+    responsiveClass: false,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: false
+  });
